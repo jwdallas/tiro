e3f0f50a1ea9b4a279c99ae83ca8122931be0f50
jwdallas <jondallas@gmail.com>
2013-10-07 20:27:05 -0700

    testing webhooks again

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
-var http = require("http");
+// requires node's http module
+var http = require('http');
 
-http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World");
-  response.end();
-}).listen(8888);
+// creates a new httpServer instance
+http.createServer(function (req, res) {
+  // this is the callback, or request handler for the httpServer
+
+  // respond to the browser, write some headers so the 
+  // browser knows what type of content we are sending
+  res.writeHead(200, {'Content-Type': 'text/html'});
+
+  // write some content to the browser that your user will see
+  res.write('<h1>hello, i know nodejitsu.</h1>');
+
+  // close the response
+  res.end();
+}).listen(8080); // the server will listen on port 8080
